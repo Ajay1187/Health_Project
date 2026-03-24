2079f2f1fda356eaea125c492d76fd2799889f87
Ajay1187 <151848592+Ajay1187@users.noreply.github.com>
2026-03-24 19:48:16 +0530

    Add Locale compatibility for mixed EnterSymptomsActivity states

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,5 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.tensorflow:tensorflow-lite:2.16.1")
 }
